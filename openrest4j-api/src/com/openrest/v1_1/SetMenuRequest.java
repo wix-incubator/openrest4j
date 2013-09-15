09966f5ae518df4eac5b48029ae4474db0657359
dleshem <dleshem@gmail.com>
2013-09-15 14:52:12 +0300

    Added SetMenuRequest.

---
+++ openrest4j-api/src/com/openrest/v1_1/SetMenuRequest.java
@@ -0,0 +1,33 @@
+package com.openrest.v1_1;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SetMenuRequest extends Request {
+	public static final String TYPE = "set_menu";
+    private static final long serialVersionUID = 1L;
+    
+    /** Default constructor for JSON deserialization. */
+    public SetMenuRequest() {}
+    
+    public SetMenuRequest(String accessToken, String organizationId, Menu menu, Boolean optimize) {
+    	this.accessToken = accessToken;
+    	this.organizationId = organizationId;
+    	this.menu = menu;
+    	this.optimize = optimize;
+    }
+    
+    @JsonInclude(Include.NON_NULL)
+    public String accessToken;
+    
+    @JsonInclude(Include.NON_NULL)
+    public String organizationId;
+    
+    @JsonInclude(Include.NON_NULL)
+    public Menu menu;
+    
+    @JsonInclude(Include.NON_DEFAULT)
+    public Boolean optimize = Boolean.FALSE;
+}
