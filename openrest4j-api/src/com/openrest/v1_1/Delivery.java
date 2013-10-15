583261038c228fe014732130571900c29ec0b95e
dleshem <dleshem@gmail.com>
2013-10-15 14:13:59 +0300

    Added Delivery.timeGuarantee

--- openrest4j-api/src/com/openrest/v1_1/Delivery.java
+++ openrest4j-api/src/com/openrest/v1_1/Delivery.java
@@ -13,6 +13,16 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 public class Delivery implements Serializable, Cloneable {
     private static final long serialVersionUID = 1L;
     
+    /** Guaranteed to happen before the given timestamp. */
+    public static final String TIME_GUARANTEE_BEFORE = "before";
+    /** Guaranteed to happen at about the given timestamp. */
+    public static final String TIME_GUARANTEE_APPROXIMATE = "about";
+    
+    /** All known time guarantees. */
+    public static final Set<String> ALL_TIME_GUARANTEES = new HashSet<String>(Arrays.asList(new String[] {
+    		TIME_GUARANTEE_BEFORE, TIME_GUARANTEE_APPROXIMATE
+    }));
+    
     /** Delivery to an address of your choice. */
     public static final String DELIVERY_TYPE_DELIVERY = "delivery";
     /** Take-out from the restaurant. */
@@ -23,10 +33,11 @@ public class Delivery implements Serializable, Cloneable {
         DELIVERY_TYPE_DELIVERY, DELIVERY_TYPE_TAKEOUT
     }));
 
-    public Delivery(String type, Address address, Long time, Integer charge) {
+    public Delivery(String type, Address address, Long time, String timeGuarantee, Integer charge) {
         this.type = type;
         this.address = address;
         this.time = time;
+        this.timeGuarantee = timeGuarantee;
         this.charge = charge;
     }
 
@@ -35,7 +46,7 @@ public class Delivery implements Serializable, Cloneable {
     
     @Override
 	public Object clone() {
-    	return new Delivery(type, ((address != null) ? (Address) address.clone() : null), time, charge);
+    	return new Delivery(type, ((address != null) ? (Address) address.clone() : null), time, timeGuarantee, charge);
 	}
 
     /** Delivery type. */
@@ -49,6 +60,10 @@ public class Delivery implements Serializable, Cloneable {
     /** Timestamp by which the order will be delivered or ready for pick-up. */
     @JsonInclude(Include.NON_NULL)
     public Long time;
+    
+    /** The type of time guarantee given. */
+    @JsonInclude(Include.NON_DEFAULT)
+    public String timeGuarantee = TIME_GUARANTEE_BEFORE;
     
     public java.util.Date time() {
         return ((time != null) ? new java.util.Date(time) : null);
