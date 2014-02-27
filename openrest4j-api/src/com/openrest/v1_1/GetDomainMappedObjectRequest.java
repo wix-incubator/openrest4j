fed8d7381664566c9352ed7535019fc158630207
dleshem <dleshem@gmail.com>
2014-02-27 11:07:41 +0200

    Deprecated GetDomainMappedObjectRequest. Use GetAppMappedObjectRequest (platform = web, id = domain, version = any)

--- openrest4j-api/src/com/openrest/v1_1/GetDomainMappedObjectRequest.java
+++ openrest4j-api/src/com/openrest/v1_1/GetDomainMappedObjectRequest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+/** Deprecated 2014-02-27, use GetAppMappedObjectRequest (platform=web, id=domain, version=any) */
+@Deprecated
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GetDomainMappedObjectRequest extends Request {
 	public static final String TYPE = "get_domain_mapped_object";
