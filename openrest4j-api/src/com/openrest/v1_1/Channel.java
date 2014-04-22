cb2d9f409a86f3560c2db017b41c77e9897f7ef1
dleshem <dleshem@gmail.com>
2014-04-22 16:00:31 +0300

    Added Channel.CHANNEL_CLOUDPRINT

--- openrest4j-api/src/com/openrest/v1_1/Channel.java
+++ openrest4j-api/src/com/openrest/v1_1/Channel.java
@@ -47,11 +47,17 @@ public class Channel {
 	 * param: Beecomm restaurant-id.
 	 */
     public static final String CHANNEL_BEECOMM = "com.beecomm";
+	/**
+	 * Google CloudPrint.
+	 * param: Google CloudPrint printer-id.
+	 */
+    public static final String CHANNEL_CLOUDPRINT = "com.google.cloudprint";
 	
     /** All known channels. */
     public static final Set<String> ALL_CHANNELS = new HashSet<String>(Arrays.asList(
     		CHANNEL_EMAIL, CHANNEL_FAX, CHANNEL_SMS, CHANNEL_VOICE, CHANNEL_RESTCLOUD,
-    		CHANNEL_FRSVISION_POS, CHANNEL_FRSVISION_BURGERANCH, CHANNEL_BEECOMM));
+    		CHANNEL_FRSVISION_POS, CHANNEL_FRSVISION_BURGERANCH, CHANNEL_BEECOMM,
+    		CHANNEL_CLOUDPRINT));
     
     /** Special channel param that's used as placeholder for the customer's details, e.g. by notifications. */
     public static final String CHANNEL_PARAM_CUSTOMER = "_customer_";
