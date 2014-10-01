b66f3c4ade5c4f1f9461e8110b4ee7c1f94e8449
dleshem <dleshem@gmail.com>
2014-10-01 11:00:01 +0300

    Added TrueCondition and FalseCondition.

--- openrest4j-api/src/com/openrest/olo/charges/conditions/Condition.java
+++ openrest4j-api/src/com/openrest/olo/charges/conditions/Condition.java
@@ -20,6 +20,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 	    include = JsonTypeInfo.As.PROPERTY,  
 	    property = "type")  
 @JsonSubTypes({
+	@Type(value = TrueCondition.class, name = AndCondition.TYPE),
+	@Type(value = FalseCondition.class, name = OrCondition.TYPE),
 	@Type(value = AndCondition.class, name = AndCondition.TYPE),
 	@Type(value = OrCondition.class, name = OrCondition.TYPE),
 	@Type(value = NotCondition.class, name = NotCondition.TYPE),
