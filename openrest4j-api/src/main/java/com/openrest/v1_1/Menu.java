e472fa5d4b5a75028682b5d4d209cf01b889aaa1
Danny Leshem <dleshem@gmail.com>
2015-03-11 17:19:22 +0200

    Menu now implements equals and hashCode

--- openrest4j-api/src/main/java/com/openrest/v1_1/Menu.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Menu.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Menu implements Serializable, Cloneable {
     private static final long serialVersionUID = 1L;
-    
+
     public Menu(Long modified, List<Item> items, List<Category> categories, List<com.openrest.olo.charges.Charge> chargesV2,
     		List<Charge> charges) {
     	this.modified = modified;
@@ -31,7 +31,35 @@ public class Menu implements Serializable, Cloneable {
     	cloned.tags = Tag.clone(tags);
     	return cloned;
 	}
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Menu menu = (Menu) o;
+
+        if (categories != null ? !categories.equals(menu.categories) : menu.categories != null) return false;
+        if (charges != null ? !charges.equals(menu.charges) : menu.charges != null) return false;
+        if (chargesV2 != null ? !chargesV2.equals(menu.chargesV2) : menu.chargesV2 != null) return false;
+        if (items != null ? !items.equals(menu.items) : menu.items != null) return false;
+        if (modified != null ? !modified.equals(menu.modified) : menu.modified != null) return false;
+        if (tags != null ? !tags.equals(menu.tags) : menu.tags != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = modified != null ? modified.hashCode() : 0;
+        result = 31 * result + (items != null ? items.hashCode() : 0);
+        result = 31 * result + (tags != null ? tags.hashCode() : 0);
+        result = 31 * result + (categories != null ? categories.hashCode() : 0);
+        result = 31 * result + (chargesV2 != null ? chargesV2.hashCode() : 0);
+        result = 31 * result + (charges != null ? charges.hashCode() : 0);
+        return result;
+    }
+
     /** The menu's last modification timestamp. */
     @JsonInclude(Include.NON_NULL)
     public Long modified;
