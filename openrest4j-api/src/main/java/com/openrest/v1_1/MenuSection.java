b4f46d6831ffb118760cc4f5005b49b39db28ee8
Danny Leshem <dleshem@gmail.com>
2015-05-10 13:50:57 +0300

    Added MenuSection.availability

--- openrest4j-api/src/main/java/com/openrest/v1_1/MenuSection.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/MenuSection.java
@@ -3,22 +3,27 @@ package com.openrest.v1_1;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.wix.restaurants.availability.Availability;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MenuSection implements Serializable, Cloneable {
     private static final long serialVersionUID = 1L;
 
     public MenuSection(String id, Map<String, String> title, Map<String, String> description,
-                       List<MenuSection> children, List<String> itemIds,
+                       List<MenuSection> children, List<String> itemIds, Availability availability,
                        Map<String, Blob> blobs, Map<String, String> properties) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.children = children;
         this.itemIds = itemIds;
+        this.availability = availability;
         this.blobs = blobs;
         this.properties = properties;
     }
@@ -33,6 +38,7 @@ public class MenuSection implements Serializable, Cloneable {
                 ((description != null) ? new LinkedHashMap<>(description) : null),
                 clone(children),
                 ((itemIds != null) ? new LinkedList<>(itemIds) : null),
+                ((availability != null) ? (Availability) availability.clone() : null),
                 ((blobs != null) ? new LinkedHashMap<>(blobs) : null),
                 ((properties != null) ? new LinkedHashMap<>(properties) : null));
     }
@@ -61,6 +67,7 @@ public class MenuSection implements Serializable, Cloneable {
         if (description != null ? !description.equals(that.description) : that.description != null) return false;
         if (children != null ? !children.equals(that.children) : that.children != null) return false;
         if (itemIds != null ? !itemIds.equals(that.itemIds) : that.itemIds != null) return false;
+        if (availability != null ? !availability.equals(that.availability) : that.availability != null) return false;
         if (blobs != null ? !blobs.equals(that.blobs) : that.blobs != null) return false;
         return !(properties != null ? !properties.equals(that.properties) : that.properties != null);
 
@@ -73,6 +80,7 @@ public class MenuSection implements Serializable, Cloneable {
         result = 31 * result + (description != null ? description.hashCode() : 0);
         result = 31 * result + (children != null ? children.hashCode() : 0);
         result = 31 * result + (itemIds != null ? itemIds.hashCode() : 0);
+        result = 31 * result + (availability != null ? availability.hashCode() : 0);
         result = 31 * result + (blobs != null ? blobs.hashCode() : 0);
         result = 31 * result + (properties != null ? properties.hashCode() : 0);
         return result;
@@ -97,6 +105,10 @@ public class MenuSection implements Serializable, Cloneable {
     /** The item-ids in this category. */
     @JsonInclude(Include.NON_DEFAULT)
     public List<String> itemIds = new LinkedList<>();
+
+    /** Time windows in which this section is available. */
+    @JsonInclude(Include.NON_DEFAULT)
+    public Availability availability = new Availability();
 
     /**
      * Maps blob-types to blobs.
