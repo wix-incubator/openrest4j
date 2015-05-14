076718b74932a82b4033a636caa6823248d45a77
Danny Leshem <dleshem@gmail.com>
2015-05-14 14:40:03 +0300

    Added SecretCreditcardsInfo.merchantKeys

--- openrest4j-api/src/main/java/com/openrest/v1_1/SecretCreditcardsInfo.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/SecretCreditcardsInfo.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SecretCreditcardsInfo implements Serializable, Cloneable {
@@ -15,16 +17,22 @@ public class SecretCreditcardsInfo implements Serializable, Cloneable {
     /** Default constructor for JSON deserialization. */
     public SecretCreditcardsInfo() {}
 
-    public SecretCreditcardsInfo(String gatewayId) {
+    public SecretCreditcardsInfo(String gatewayId, Map<String, String> merchantKeys) {
         this.gatewayId = gatewayId;
+        this.merchantKeys = merchantKeys;
     }
 
     @Override
     public Object clone() {
-        return new SecretCreditcardsInfo(gatewayId);
+        return new SecretCreditcardsInfo(gatewayId,
+                ((merchantKeys != null) ? new LinkedHashMap<>(merchantKeys) : null));
     }
 
     /** Payment gateway to use for credit card clearing (null means manual clearing). */
     @JsonInclude(Include.NON_NULL)
     public String gatewayId;
+
+    /** Maps payment gateways to their opaque merchant keys. */
+    @JsonInclude(Include.NON_DEFAULT)
+    public Map<String, String> merchantKeys = new LinkedHashMap<>();
 }
