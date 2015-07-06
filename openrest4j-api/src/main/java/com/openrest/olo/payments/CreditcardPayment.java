3c41fa33bd6bde0aa465d58ae9c61b0f76c06c80
Danny Leshem <dleshem@gmail.com>
2015-07-06 11:55:51 +0300

    Added CreditcardPayment.collectionMethod.

--- openrest4j-api/src/main/java/com/openrest/olo/payments/CreditcardPayment.java
+++ openrest4j-api/src/main/java/com/openrest/olo/payments/CreditcardPayment.java
@@ -17,11 +17,13 @@ public class CreditcardPayment extends Payment {
     /** Default constructor for JSON deserialization. */
     public CreditcardPayment() {}
 
-    public CreditcardPayment(Integer amount, Map<String, String> externalIds, CreditCard card, CreditCardToken cardToken,
-                             String userId, String id, String password) {
+    public CreditcardPayment(Integer amount, Map<String, String> externalIds,
+                             String collectionMethod, CreditCardToken cardToken,
+                             CreditCard card, String userId, String id, String password) {
         super(amount, externalIds);
-        this.card = card;
+        this.collectionMethod = collectionMethod;
         this.cardToken = cardToken;
+        this.card = card;
         this.userId = userId;
         this.id = id;
         this.password = password;
@@ -31,8 +33,9 @@ public class CreditcardPayment extends Payment {
     public Object clone() {
         return new CreditcardPayment(amount,
                 ((externalIds != null) ? new LinkedHashMap<>(externalIds) : null),
-                ((card != null) ? (CreditCard) card.clone() : null),
+                collectionMethod,
                 cardToken,
+                ((card != null) ? (CreditCard) card.clone() : null),
                 userId, id, password);
     }
 
@@ -45,8 +48,9 @@ public class CreditcardPayment extends Payment {
 
         if (amount != null ? !amount.equals(that.amount) : that.amount != null) return false;
         if (externalIds != null ? !externalIds.equals(that.externalIds) : that.externalIds != null) return false;
-        if (card != null ? !card.equals(that.card) : that.card != null) return false;
+        if (collectionMethod != null ? !collectionMethod.equals(that.collectionMethod) : that.collectionMethod != null) return false;
         if (cardToken != null ? !cardToken.equals(that.cardToken) : that.cardToken != null) return false;
+        if (card != null ? !card.equals(that.card) : that.card != null) return false;
         if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
         if (id != null ? !id.equals(that.id) : that.id != null) return false;
         return !(password != null ? !password.equals(that.password) : that.password != null);
@@ -57,21 +61,29 @@ public class CreditcardPayment extends Payment {
     public int hashCode() {
         int result = amount != null ? amount.hashCode() : 0;
         result = 31 * result + (externalIds != null ? externalIds.hashCode() : 0);
-        result = 31 * result + (card != null ? card.hashCode() : 0);
+        result = 31 * result + (collectionMethod != null ? collectionMethod.hashCode() : 0);
         result = 31 * result + (cardToken != null ? cardToken.hashCode() : 0);
+        result = 31 * result + (card != null ? card.hashCode() : 0);
         result = 31 * result + (userId != null ? userId.hashCode() : 0);
         result = 31 * result + (id != null ? id.hashCode() : 0);
         result = 31 * result + (password != null ? password.hashCode() : 0);
         return result;
     }
 
+    /**
+     * Credit card collection method.
+     * @see com.openrest.v1_1.CreditcardsInfo#ALL_COLLECTION_METHODS
+     */
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public String collectionMethod;
+
+    /** Card token (if card is collected online). */
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public CreditCardToken cardToken;
+
     /** Credit card details (will be deprecated 2015-08-01). */
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public CreditCard card;
-
-    /** Credit card details */
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public CreditCardToken cardToken;
 
     /** The user's Facebook id (for saved payments). */
     @JsonInclude(JsonInclude.Include.NON_NULL)
