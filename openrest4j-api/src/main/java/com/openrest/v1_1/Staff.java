4753b6e651d3444a4b69a22bb21ef5e9abe3b33a
Danny Leshem <dleshem@gmail.com>
2015-08-30 16:13:24 +0300

    Staff now consists of sets of IDs (previously: lists).

--- openrest4j-api/src/main/java/com/openrest/v1_1/Staff.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Staff.java
@@ -1,19 +1,12 @@
 package com.openrest.v1_1;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.Map.Entry;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Staff implements Serializable, Cloneable {
@@ -25,11 +18,11 @@ public class Staff implements Serializable, Cloneable {
     public static final String STAFF_EMPLOYEE = "employee";
     
     /** All known staff types. */
-    public static final Set<String> ALL_STAFFS = new HashSet<String>(Arrays.asList(new String[] {
+    public static final Set<String> ALL_STAFFS = new HashSet<>(Arrays.asList(new String[] {
    		STAFF_ADMIN, STAFF_DISTRIBUTOR, STAFF_MANAGER, STAFF_EMPLOYEE
     }));
 
-    public Staff(Map<String, List<String>> staff) {
+    public Staff(Map<String, Set<String>> staff) {
         this.staff = staff;
     }
 
@@ -38,11 +31,11 @@ public class Staff implements Serializable, Cloneable {
     
     @Override
 	public Object clone() {
-    	final Map<String, List<String>> clonedStaff;
+    	final Map<String, Set<String>> clonedStaff;
     	if (staff != null) {
-    		clonedStaff = new LinkedHashMap<String, List<String>>(staff.size());
-    		for (Entry<String, List<String>> entry : staff.entrySet()) {
-    			clonedStaff.put(entry.getKey(), new ArrayList<String>(entry.getValue()));
+    		clonedStaff = new LinkedHashMap<String, Set<String>>(staff.size());
+    		for (Entry<String, Set<String>> entry : staff.entrySet()) {
+    			clonedStaff.put(entry.getKey(), new LinkedHashSet<String>(entry.getValue()));
     		}
     	} else {
     		clonedStaff = null;
@@ -51,38 +44,38 @@ public class Staff implements Serializable, Cloneable {
     	return new Staff(clonedStaff);
 	}
 
-    public static Staff createAdmins(List<String> adminStaff) {
-        final Map<String, List<String>> staff = new HashMap<String, List<String>>(1);
+    public static Staff createAdmins(Set<String> adminStaff) {
+        final Map<String, Set<String>> staff = new HashMap<>(1);
         staff.put(STAFF_ADMIN, adminStaff);
         return new Staff(staff);
     }
     
-    public static Staff createDistributors(List<String> distributorStaff) {
-        final Map<String, List<String>> staff = new HashMap<String, List<String>>(1);
+    public static Staff createDistributors(Set<String> distributorStaff) {
+        final Map<String, Set<String>> staff = new LinkedHashMap<>(1);
         staff.put(STAFF_DISTRIBUTOR, distributorStaff);
         return new Staff(staff);
     }
     
-    public static Staff createManagers(List<String> managerStaff) {
-        final Map<String, List<String>> staff = new HashMap<String, List<String>>(1);
+    public static Staff createManagers(Set<String> managerStaff) {
+        final Map<String, Set<String>> staff = new LinkedHashMap<>(1);
         staff.put(STAFF_MANAGER, managerStaff);
         return new Staff(staff);
     }
 
-    public static Staff createEmployees(List<String> employeeStaff) {
-        final Map<String, List<String>> staff = new HashMap<String, List<String>>(1);
+    public static Staff createEmployees(Set<String> employeeStaff) {
+        final Map<String, Set<String>> staff = new LinkedHashMap<>(1);
         staff.put(STAFF_EMPLOYEE, employeeStaff);
         return new Staff(staff);
     }
 
-    public static Staff createRestaurantStaff(List<String> managerStaff, List<String> employeeStaff) {
-        final Map<String, List<String>> staff = new HashMap<String, List<String>>(2);
+    public static Staff createRestaurantStaff(Set<String> managerStaff, Set<String> employeeStaff) {
+        final Map<String, Set<String>> staff = new LinkedHashMap<>(2);
         staff.put(STAFF_MANAGER, managerStaff);
         staff.put(STAFF_EMPLOYEE, employeeStaff);
         return new Staff(staff);
     }
 
-    /** Maps roles (see above) to list of user-ids. */
+    /** Maps roles (see above) to a set of user-ids. */
     @JsonInclude(Include.NON_DEFAULT)
-    public Map<String, List<String>> staff = new HashMap<String, List<String>>();
+    public Map<String, Set<String>> staff = new LinkedHashMap<>();
 }
