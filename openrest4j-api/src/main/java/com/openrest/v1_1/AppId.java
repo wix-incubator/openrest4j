2066caeef582e6cb4ed7012ac2f800906f2333c4
Danny Leshem <dleshem@gmail.com>
2015-12-07 18:41:13 +0200

    AppId hashCode, equals

--- openrest4j-api/src/main/java/com/openrest/v1_1/AppId.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/AppId.java
@@ -57,6 +57,32 @@ public class AppId implements Serializable, Cloneable {
     	return new AppId(platform, id, version);
 	}
 
+    @Override
+    public String toString() {
+        return "AppId [platform=" + platform + ", id=" + id + ", version=" + version + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AppId appId = (AppId) o;
+
+        if (platform != null ? !platform.equals(appId.platform) : appId.platform != null) return false;
+        if (id != null ? !id.equals(appId.id) : appId.id != null) return false;
+        return !(version != null ? !version.equals(appId.version) : appId.version != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = platform != null ? platform.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + (version != null ? version.hashCode() : 0);
+        return result;
+    }
+
     /** Application platform (@see ALL_PLATFORMS) */
     @JsonInclude(Include.NON_NULL)
     public String platform;
@@ -71,9 +97,4 @@ public class AppId implements Serializable, Cloneable {
     /** The application's version. */
     @JsonInclude(Include.NON_NULL)
     public String version;
-    
-	@Override
-	public String toString() {
-		return "AppId [platform=" + platform + ", id=" + id + ", version=" + version + "]";
-	}
 }
