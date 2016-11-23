9407002aafdcae3512527041f3ba596f9b8333a3
Danny Leshem <dleshem@gmail.com>
2016-11-23 11:23:40 +0200

    Constified constants.

--- openrest4j-api/src/main/java/com/wix/restaurants/reservations/Statuses.java
+++ openrest4j-api/src/main/java/com/wix/restaurants/reservations/Statuses.java
@@ -8,14 +8,14 @@ public class Statuses {
      * Further confirmation is required before the reservation is submitted, e.g. validating the customer's phone
      * number by SMS.
      */
-    public static String pending = "pending";
+    public static final String pending = "pending";
 
     /** The reservation has been submitted, and awaits approval. */
-    public static String new_ = "new";
+    public static final String new_ = "new";
 
     /** The reservation has been processed and accepted. */
-    public static String accepted = "accepted";
+    public static final String accepted = "accepted";
 
     /** The reservation has been canceled. */
-    public static String canceled = "canceled";
+    public static final String canceled = "canceled";
 }
