dced2af4033e8221379464259a72daf8867ff880
Danny Leshem <dleshem@gmail.com>
2016-12-19 16:22:42 +0200

    api.openrest.com -> api.wixrestaurants.com

--- openrest4j-client/src/main/java/com/openrest/v1_1/Endpoints.java
+++
@@ -1,5 +0,0 @@
-package com.openrest.v1_1;
-
-public class Endpoints {
-    public static final String PRODUCTION = "https://api.openrest.com/v1.1";
-}
---
+++ openrest4j-client/src/main/java/com/wix/restaurants/Endpoints.java
@@ -0,0 +1,7 @@
+package com.wix.restaurants;
+
+public class Endpoints {
+    private Endpoints() {}
+
+    public static final String production = "https://api.wixrestaurants.com/v1.1";
+}
