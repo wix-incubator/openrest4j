d0de35aac609fdbfe5a91b9a433ee76e46c82950
Danny Leshem <dleshem@gmail.com>
2017-01-10 18:00:58 +0200

    ArrayList -> LinkedList

--- openrest4j-api/src/main/java/com/openrest/v1_1/StatsResponse.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/StatsResponse.java
@@ -1,11 +1,11 @@
 package com.openrest.v1_1;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
 public class StatsResponse implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -18,5 +18,5 @@ public class StatsResponse implements Serializable {
     }
 
     @JsonInclude(Include.NON_DEFAULT)
-	public List<Stats> results = new ArrayList<Stats>();
+	public List<Stats> results = new LinkedList<>();
 }
