8fac7f59a8f8b156b9547c2fa0cbdb8a3870eb16
Danny Leshem <dleshem@gmail.com>
2017-01-10 15:36:39 +0200

    Long -> Date

--- openrest4j-api/src/main/java/com/openrest/v1_1/Property.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Property.java
@@ -1,16 +1,17 @@
 package com.openrest.v1_1;
 
-import java.io.Serializable;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import java.io.Serializable;
+import java.util.Date;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Property implements Serializable, Cloneable {
     private static final long serialVersionUID = 1L;
     
-	public Property(String id, String value, Long modified) {
+	public Property(String id, String value, Date modified) {
         this.id = id;
         this.value = value;
         this.modified = modified;
@@ -21,7 +22,8 @@ public class Property implements Serializable, Cloneable {
     
     @Override
 	public Object clone() {
-    	return new Property(id, value, modified);
+    	return new Property(id, value,
+                (modified != null) ? (Date) modified.clone() : null);
 	}
 
     @JsonInclude(Include.NON_NULL)
@@ -31,5 +33,5 @@ public class Property implements Serializable, Cloneable {
     public String value;
 
     @JsonInclude(Include.NON_NULL)
-    public Long modified;
+    public Date modified;
 }
