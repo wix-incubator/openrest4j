4517a38fdc46f68250b93cfb4529c6b7dc65af62
Danny Leshem <dleshem@gmail.com>
2017-07-30 16:02:42 +0300

    Added hashCode and equals to Viewport.

--- openrest4j-api/src/main/java/com/openrest/v1_1/Viewport.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Viewport.java
@@ -1,10 +1,10 @@
 package com.openrest.v1_1;
 
-import java.io.Serializable;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Viewport implements Serializable {
@@ -18,7 +18,27 @@ public class Viewport implements Serializable {
     
     /** Default constructor for JSON deserialization. */
     public Viewport() {}
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Viewport viewport = (Viewport) o;
+
+        if (width != null ? !width.equals(viewport.width) : viewport.width != null) return false;
+        if (initialScale != null ? !initialScale.equals(viewport.initialScale) : viewport.initialScale != null) return false;
+        return maximumScale != null ? maximumScale.equals(viewport.maximumScale) : viewport.maximumScale == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = width != null ? width.hashCode() : 0;
+        result = 31 * result + (initialScale != null ? initialScale.hashCode() : 0);
+        result = 31 * result + (maximumScale != null ? maximumScale.hashCode() : 0);
+        return result;
+    }
+
     @JsonInclude(Include.NON_NULL)
     public Integer width; 
     
