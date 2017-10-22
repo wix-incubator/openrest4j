2b415eb27768d93694119b5080ea074005abade2
Danny Leshem <dleshem@gmail.com>
2017-10-22 14:29:25 +0300

    Added NotifyReservationRequest.

--- openrest4j-api/src/main/java/com/wix/restaurants/requests/Request.java
+++ openrest4j-api/src/main/java/com/wix/restaurants/requests/Request.java
@@ -111,6 +111,7 @@ import java.io.Serializable;
 	@Type(value = QueryReservationsRequest.class, name = QueryReservationsRequest.TYPE),
 	@Type(value = SetReservationStatusRequest.class, name = SetReservationStatusRequest.TYPE),
 	@Type(value = ConfirmReservationRequest.class, name = ConfirmReservationRequest.TYPE),
+	@Type(value = NotifyReservationRequest.class, name = NotifyReservationRequest.TYPE),
 	@Type(value = JoinChainRequest.class, name = JoinChainRequest.TYPE),
 	@Type(value = GetRoleRequest.class, name = GetRoleRequest.TYPE),
 	@Type(value = SendDashboardAppRequest.class, name = SendDashboardAppRequest.TYPE)
