24a94f092669fc868f5e767e87f7a5ca8e3ad539
Danny Leshem <dleshem@gmail.com>
2017-10-24 11:34:08 +0300

    Added OrderInfo.maxCommentLength

--- openrest4j-api/src/main/java/com/wix/restaurants/orders/OrdersInfo.java
+++ openrest4j-api/src/main/java/com/wix/restaurants/orders/OrdersInfo.java
@@ -20,10 +20,12 @@ public class OrdersInfo implements Serializable, Cloneable {
     public OrdersInfo(Availability availability,
                       AsapOrdersInfo asap,
                       FutureOrdersInfo future,
+                      Integer maxCommentLength,
                       Map<String, String> properties) {
         this.availability = availability;
         this.asap = asap;
         this.future = future;
+        this.maxCommentLength = maxCommentLength;
         this.properties = properties;
     }
 
@@ -33,6 +35,7 @@ public class OrdersInfo implements Serializable, Cloneable {
                 (availability != null) ? (Availability) availability.clone() : null,
                 (asap != null) ? (AsapOrdersInfo) asap.clone() : null,
                 (future != null) ? (FutureOrdersInfo) future.clone() : null,
+                maxCommentLength,
                 ((properties != null) ? new LinkedHashMap<>(properties) : null));
     }
 
@@ -46,6 +49,7 @@ public class OrdersInfo implements Serializable, Cloneable {
         if (availability != null ? !availability.equals(that.availability) : that.availability != null) return false;
         if (asap != null ? !asap.equals(that.asap) : that.asap != null) return false;
         if (future != null ? !future.equals(that.future) : that.future != null) return false;
+        if (maxCommentLength != null ? !maxCommentLength.equals(that.maxCommentLength) : that.maxCommentLength != null) return false;
         return properties != null ? properties.equals(that.properties) : that.properties == null;
 
     }
@@ -55,6 +59,7 @@ public class OrdersInfo implements Serializable, Cloneable {
         int result = availability != null ? availability.hashCode() : 0;
         result = 31 * result + (asap != null ? asap.hashCode() : 0);
         result = 31 * result + (future != null ? future.hashCode() : 0);
+        result = 31 * result + (maxCommentLength != null ? maxCommentLength.hashCode() : 0);
         result = 31 * result + (properties != null ? properties.hashCode() : 0);
         return result;
     }
@@ -70,6 +75,15 @@ public class OrdersInfo implements Serializable, Cloneable {
     /** Settings for future orders. */
     @JsonInclude(Include.NON_NULL)
     public FutureOrdersInfo future;
+
+    /**
+     * Maximum allowed length (in characters) for order comment, or null if unlimited.
+     * A value of 0 means order comments are disabled.
+     *
+     * @see com.openrest.v1_1.Order#comment
+     */
+    @JsonInclude(Include.NON_NULL)
+    public Integer maxCommentLength;
 
     /**
      * Map of user-defined extended properties.
