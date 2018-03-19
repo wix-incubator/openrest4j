44e3e8261d2e9ab1111c7b1aabedfcbeef1b38ac
Danny Leshem <dleshem@gmail.com>
2018-03-19 13:14:26 +0200

    Organizations: hashCode, equals, toString

--- openrest4j-api/src/main/java/com/openrest/v1_1/Organizations.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Organizations.java
@@ -1,11 +1,12 @@
 package com.openrest.v1_1;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.Objects;
 
 public class Organizations implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -15,6 +16,27 @@ public class Organizations implements Serializable {
 	
     public Organizations(List<Organization> organizations) {
     	this.organizations = organizations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organizations that = (Organizations) o;
+        return Objects.equals(organizations, that.organizations);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(organizations);
+    }
+
+    @Override
+    public String toString() {
+        return "Organizations{" +
+                "organizations=" + organizations +
+                '}';
     }
 
     @JsonInclude(Include.NON_DEFAULT)
