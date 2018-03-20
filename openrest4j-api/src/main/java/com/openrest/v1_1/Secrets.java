094c693412d64cd5ceaf2a162ea68b76fe237bf2
Danny Leshem <dleshem@gmail.com>
2018-03-20 15:38:25 +0200

    Secrets: equals, hashCode, toString, clone return value

--- openrest4j-api/src/main/java/com/openrest/v1_1/Secrets.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Secrets.java
@@ -6,9 +6,9 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Secrets implements Serializable, Cloneable {
@@ -24,12 +24,36 @@ public class Secrets implements Serializable, Cloneable {
     public Secrets() {}
     
     @Override
-	public Object clone() {
+	public Secrets clone() {
     	return new Secrets(
                 (modified != null) ? (Date) modified.clone() : null,
-    			((properties != null) ? new HashMap<String, String>(properties) : null),
+    			((properties != null) ? new LinkedHashMap<>(properties) : null),
     			anonymized);
 	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Secrets secrets = (Secrets) o;
+        return Objects.equals(modified, secrets.modified) &&
+                Objects.equals(properties, secrets.properties) &&
+                Objects.equals(anonymized, secrets.anonymized);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(modified, properties, anonymized);
+    }
+
+    @Override
+    public String toString() {
+        return "Secrets{" +
+                "modified=" + modified +
+                ", properties=" + properties +
+                ", anonymized=" + anonymized +
+                '}';
+    }
 
     /** Last modification timestamp. */
     @JsonInclude(Include.NON_NULL)
