f81a391e52fe884886bd1396b1902b458f3ab779
Danny Leshem <dleshem@gmail.com>
2018-07-25 12:36:23 +0300

    Removed redundant fields in Distributor: facebookAppId, tld

--- openrest4j-api/src/main/java/com/openrest/v1_1/Distributor.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Distributor.java
@@ -1,8 +1,6 @@
 package com.openrest.v1_1;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.wix.restaurants.availability.Availability;
 import com.wix.restaurants.i18n.Locale;
 import com.wix.restaurants.i18n.LocalizedString;
@@ -23,15 +21,11 @@ public class Distributor extends Organization {
 					   Locale locale, Set<Locale> locales, Map<String, LocalizedString> messages,
 					   Contact contact, Address address, String timezone, String currency,
 					   List<AppInfo> apps, Seo seo,
-					   Map<String, String> properties,
-					   Map<String, Availability> availabilities, String facebookAppId, String tld,
+					   Map<String, String> properties, Map<String, Availability> availabilities,
 					   Map<String, String> media, Boolean closed, Set<Product> products) {
     	super(id, alias, affiliateId, externalIds, created, modified, title, description, locale, locales, messages,
     			contact, address, timezone, currency, apps, seo, properties,
 				availabilities, media, closed, products);
-    	
-    	this.facebookAppId = facebookAppId;
-    	this.tld = tld;
     }
 
     @Override
@@ -56,43 +50,7 @@ public class Distributor extends Organization {
 				((seo != null) ? (Seo) seo.clone() : null),
 				((properties != null) ? new LinkedHashMap<>(properties) : null),
 				Availability.clone(availabilities),
-				facebookAppId, tld,
 				((media != null) ? new LinkedHashMap<>(media) : null),
 				closed, Product.clone(products));
 	}
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        Distributor that = (Distributor) o;
-
-        if (facebookAppId != null ? !facebookAppId.equals(that.facebookAppId) : that.facebookAppId != null) return false;
-        return tld != null ? tld.equals(that.tld) : that.tld == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (facebookAppId != null ? facebookAppId.hashCode() : 0);
-        result = 31 * result + (tld != null ? tld.hashCode() : 0);
-        return result;
-    }
-
-    /**
-     * The distributor's Facebook application id.
-     * Deprecated, to be removed on 2014-08-01 (use externalIds["com.facebook.apps"])
-     */
-    @Deprecated
-    @JsonInclude(Include.NON_NULL)
-    public String facebookAppId;
-    
-    /**
-     * The distributor's top-level domain.
-     * White-label systems use domains of the form {alias}.{tld}).
-     */
-    @JsonInclude(Include.NON_NULL)
-    public String tld;
 }
