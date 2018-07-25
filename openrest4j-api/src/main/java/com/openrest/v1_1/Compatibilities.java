c7f81c10e6a48da0f0c489754239d63a6af33d74
Danny Leshem <dleshem@gmail.com>
2018-07-25 12:23:49 +0300

    Removed redundant code.

--- openrest4j-api/src/main/java/com/openrest/v1_1/Compatibilities.java
+++
@@ -1,8 +0,0 @@
-package com.openrest.v1_1;
-
-public class Compatibilities {
-    private Compatibilities() {}
-
-    public static final String CHARGES_KEY = "com.openrest.olo.charges";
-    public static final String CHARGES_V1 = "1.0.0";
-}
