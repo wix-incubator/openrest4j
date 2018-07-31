32267c548be88ab9795a08b9e20d7795956adcaf
Danny Leshem <dleshem@gmail.com>
2018-07-31 15:20:00 +0300

    Updated LatLng's hashCode, equals.

--- openrest4j-api/src/main/java/com/openrest/v1_1/LatLng.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/LatLng.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LatLng implements Serializable, Cloneable {
@@ -24,31 +25,19 @@ public class LatLng implements Serializable, Cloneable {
 	}
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final LatLng other = (LatLng) obj;
-        if (this.lat != other.lat && (this.lat == null || !this.lat.equals(other.lat))) {
-            return false;
-        }
-        if (this.lng != other.lng && (this.lng == null || !this.lng.equals(other.lng))) {
-            return false;
-        }
-        return true;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LatLng latLng = (LatLng) o;
+        return Objects.equals(lat, latLng.lat) &&
+                Objects.equals(lng, latLng.lng);
     }
 
     @Override
     public int hashCode() {
-        int hash = 3;
-        hash = 29 * hash + (this.lat != null ? this.lat.hashCode() : 0);
-        hash = 29 * hash + (this.lng != null ? this.lng.hashCode() : 0);
-        return hash;
+        return Objects.hash(lat, lng);
     }
-    
+
     @Override
     public String toString() {
         return "LatLng{" +
