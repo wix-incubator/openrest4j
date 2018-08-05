116e572dfe152852a6d6162f3976c8c5ca0b1bae
Danny Leshem <dleshem@gmail.com>
2018-08-05 11:18:41 +0300

    Beautified Contact hashCode, equals.

--- openrest4j-api/src/main/java/com/openrest/v1_1/Contact.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Contact.java
@@ -5,10 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import java.io.Serializable;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -57,6 +54,23 @@ public class Contact implements Serializable, Cloneable {
         return cloned;
     }
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Contact contact = (Contact) o;
+		return Objects.equals(firstName, contact.firstName) &&
+				Objects.equals(lastName, contact.lastName) &&
+				Objects.equals(email, contact.email) &&
+				Objects.equals(phone, contact.phone) &&
+				Objects.equals(fax, contact.fax);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(firstName, lastName, email, phone, fax);
+	}
+
     @JsonInclude(Include.NON_NULL)
     public String firstName;
 
@@ -71,55 +85,4 @@ public class Contact implements Serializable, Cloneable {
     
     @JsonInclude(Include.NON_NULL)
     public String fax;
-    
-    @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((fax == null) ? 0 : fax.hashCode());
-		result = prime * result
-				+ ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result
-				+ ((lastName == null) ? 0 : lastName.hashCode());
-		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Contact other = (Contact) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (fax == null) {
-			if (other.fax != null)
-				return false;
-		} else if (!fax.equals(other.fax))
-			return false;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		if (phone == null) {
-			if (other.phone != null)
-				return false;
-		} else if (!phone.equals(other.phone))
-			return false;
-		return true;
-	}
 }
