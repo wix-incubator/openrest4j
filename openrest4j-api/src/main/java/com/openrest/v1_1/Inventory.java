b1823fdc2348434efe7af687ebd04770be978f6b
Danny Leshem <dleshem@gmail.com>
2018-08-07 16:36:32 +0300

    Fixed Inventory.clone returned type.

--- openrest4j-api/src/main/java/com/openrest/v1_1/Inventory.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/Inventory.java
@@ -1,12 +1,12 @@
 package com.openrest.v1_1;
 
-import java.io.Serializable;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Inventory implements Serializable, Cloneable {
@@ -20,11 +20,11 @@ public class Inventory implements Serializable, Cloneable {
     }
     
     @Override
-	public Object clone() {
+	public Inventory clone() {
     	return new Inventory(Stock.clone(stocks));
 	}
     
     /** Maps item ids to number of items in stock (null means inventory is not managed for this item). */
     @JsonInclude(Include.NON_DEFAULT)
-    public Map<String, Stock> stocks = new LinkedHashMap<String, Stock>();
+    public Map<String, Stock> stocks = new LinkedHashMap<>();
 }
