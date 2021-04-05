1c30c0453deb1bf1d5b1256752243827c6663c78
NIr-Wiener <nirw@wix.com>
2021-04-05 12:44:00 +0300

    update doc

--- openrest4j-api/src/main/java/com/wix/restaurants/notifications/NewOrganizationNotification.java
+++ openrest4j-api/src/main/java/com/wix/restaurants/notifications/NewOrganizationNotification.java
@@ -2,21 +2,21 @@ package com.wix.restaurants.notifications;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-/** Triggered when an organization is changed in some way. */
+/** Triggered when an new organization is added. */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NewOrganizationNotification extends Notification {
     public static final String TYPE = "new_organization";
     private static final long serialVersionUID = 1L;
 
     /** Default constructor for JSON deserialization. */
-    public UpdatedOrganizationNotification() {}
+    public NewOrganizationNotification() {}
 
-    public UpdatedOrganizationNotification(String channelId, String channelParam, String comment, String state) {
+    public NewOrganizationNotification(String channelId, String channelParam, String comment, String state) {
         super(channelId, channelParam, comment, state);
     }
 
     @Override
-    public UpdatedOrganizationNotification clone() {
-        return new UpdatedOrganizationNotification(channelId, channelParam, comment, state);
+    public NewOrganizationNotification clone() {
+        return new NewOrganizationNotification(channelId, channelParam, comment, state);
     }
 }
