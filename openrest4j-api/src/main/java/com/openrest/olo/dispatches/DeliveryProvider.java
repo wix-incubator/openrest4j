9713d6a40fd9b873a3ffebf66d409d7d277c2bce
NIr-Wiener <nirw@wix.com>
2021-10-11 10:56:15 +0300

    adding delivery id

--- openrest4j-api/src/main/java/com/openrest/olo/dispatches/DeliveryProvider.java
+++ openrest4j-api/src/main/java/com/openrest/olo/dispatches/DeliveryProvider.java
@@ -15,16 +15,18 @@ public class DeliveryProvider implements Serializable, Cloneable {
     /** Default constructor for JSON deserialization. */
     public DeliveryProvider() {}
 
-    public DeliveryProvider(String configurationId, String estimateId, Integer restaurantSubsidyFee, Date pickupTime) {
+    public DeliveryProvider(String configurationId, String estimateId, Integer restaurantSubsidyFee, Date pickupTime, String providerName, String providerDeliveryId) {
         this.configurationId = configurationId;
         this.estimateId = estimateId;
         this.restaurantSubsidyFee = restaurantSubsidyFee;
         this.pickupTime = pickupTime;
+        this.providerName = providerName;
+        this.providerDeliveryId = providerDeliveryId;
     }
 
     @Override
     public DeliveryProvider clone() {
-        return new DeliveryProvider(configurationId, estimateId, restaurantSubsidyFee, pickupTime);
+        return new DeliveryProvider(configurationId, estimateId, restaurantSubsidyFee, pickupTime, providerName, providerDeliveryId);
     }
 
     @Override
@@ -33,14 +35,16 @@ public class DeliveryProvider implements Serializable, Cloneable {
         if (o == null || getClass() != o.getClass()) return false;
         DeliveryProvider that = (DeliveryProvider) o;
         return Objects.equals(configurationId, that.configurationId) &&
-               Objects.equals(estimateId, that.estimateId) &&
-               Objects.equals(restaurantSubsidyFee, that.restaurantSubsidyFee) &&
-               Objects.equals(pickupTime, that.pickupTime);
+                Objects.equals(estimateId, that.estimateId) &&
+                Objects.equals(restaurantSubsidyFee, that.restaurantSubsidyFee) &&
+                Objects.equals(pickupTime, that.pickupTime) &&
+                Objects.equals(providerName, that.providerName) &&
+                Objects.equals(providerDeliveryId, that.providerDeliveryId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(configurationId, estimateId, restaurantSubsidyFee, pickupTime);
+        return Objects.hash(configurationId, estimateId, restaurantSubsidyFee, pickupTime, providerName, providerDeliveryId);
     }
 
     @JsonInclude(Include.NON_NULL)
@@ -50,8 +54,14 @@ public class DeliveryProvider implements Serializable, Cloneable {
     public String estimateId;
 
     @JsonInclude(Include.NON_NULL)
+    public String providerDeliveryId;
+
+    @JsonInclude(Include.NON_NULL)
     public Integer restaurantSubsidyFee = 0;
 
     @JsonInclude(Include.NON_NULL)
     public Date pickupTime;
+
+    @JsonInclude(Include.NON_NULL)
+    public String providerName;
 }
