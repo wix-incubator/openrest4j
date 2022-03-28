9c8a6e3779f38145181fe1d287343d02cafbedca
InterAl <interal@gmail.com>
2022-03-29 01:42:10 +0300

    posInfo: additional fields

--- openrest4j-api/src/main/java/com/openrest/v1_1/PosInfo.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/PosInfo.java
@@ -14,14 +14,17 @@ public class PosInfo implements Serializable, Cloneable {
     /** Default constructor for JSON deserialization. */
     public PosInfo() {}
 
-    public PosInfo(String posProviderId, String posOrderId) {
+    public PosInfo(String posProviderId, String posOrderId, boolean isPreOrder, String paymentGatewayId, String cashierChargeProviderIdKey) {
         this.posProviderId = posProviderId;
         this.posOrderId = posOrderId;
+        this.isPreOrder = isPreOrder;
+        this.paymentGatewayId = paymentGatewayId;
+        this.cashierChargeProviderIdKey = cashierChargeProviderIdKey;
     }
 
     @Override
     public PosInfo clone() {
-        return new PosInfo(posProviderId, posOrderId);
+        return new PosInfo(posProviderId, posOrderId, isPreOrder, paymentGatewayId, cashierChargeProviderIdKey);
     }
 
     @Override
@@ -30,12 +33,15 @@ public class PosInfo implements Serializable, Cloneable {
         if (o == null || getClass() != o.getClass()) return false;
         PosInfo that = (PosInfo) o;
         return Objects.equals(posProviderId, that.posProviderId) &&
-               Objects.equals(posOrderId, that.posOrderId);
+               Objects.equals(posOrderId, that.posOrderId) &&
+               Objects.equals(isPreOrder, that.isPreOrder) &&
+               Objects.equals(paymentGatewayId, that.paymentGatewayId) &&
+               Objects.equals(cashierChargeProviderIdKey, that.cashierChargeProviderIdKey);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(posProviderId, posOrderId);
+        return Objects.hash(posProviderId, posOrderId, isPreOrder, paymentGatewayId, cashierChargeProviderIdKey);
     }
 
     @JsonInclude(Include.NON_NULL)
@@ -43,4 +49,13 @@ public class PosInfo implements Serializable, Cloneable {
 
     @JsonInclude(Include.NON_NULL)
     public String posOrderId;
+
+    @JsonInclude(Include.NON_NULL)
+    public boolean isPreOrder;
+
+    @JsonInclude(Include.NON_NULL)
+    public String paymentGatewayId;
+
+    @JsonInclude(Include.NON_NULL)
+    public String cashierChargeProviderIdKey;
 }
