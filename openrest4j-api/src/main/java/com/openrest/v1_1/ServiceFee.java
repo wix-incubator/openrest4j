60c8c17b597587c982e0664fd7171781c9b1b6ed
guyb <57596601+guywix@users.noreply.github.com>
2022-11-27 11:24:23 +0200

    removed unnecessary fields

--- openrest4j-api/src/main/java/com/openrest/v1_1/ServiceFee.java
+++ openrest4j-api/src/main/java/com/openrest/v1_1/ServiceFee.java
@@ -3,10 +3,8 @@ package com.openrest.v1_1;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.wix.restaurants.fees.Value;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -16,21 +14,15 @@ public class ServiceFee implements Serializable, Cloneable {
     /** Default constructor for JSON deserialization. */
     public ServiceFee() {}
 
-    public ServiceFee(String id, String locationId, String name, Date created, Double taxRate,
-     Boolean enabled, String label, Value value) {
+    public ServiceFee(String id, String name, String amount) {
         this.id = id;
-        this.locationId = locationId;
         this.name = name;
-        this.created = created;
-        this.taxRate = taxRate;
-        this.enabled = enabled;
-        this.label = label;
-        this.value = value;
+        this.amount = amount;
     }
 
     @Override
     public ServiceFee clone() {
-        return new ServiceFee(id, locationId, name, created, taxRate, enabled, label, value);
+        return new ServiceFee(id, name, amount);
     }
 
     @Override
@@ -40,49 +32,24 @@ public class ServiceFee implements Serializable, Cloneable {
 
         ServiceFee serviceFee = (ServiceFee) o;
         return Objects.equals(id, serviceFee.id) &&
-                Objects.equals(locationId, serviceFee.locationId) &&
                 Objects.equals(name, serviceFee.name) &&
-                Objects.equals(created, serviceFee.created) &&
-                Objects.equals(taxRate, serviceFee.taxRate) &&
-                Objects.equals(enabled, serviceFee.enabled) &&
-                Objects.equals(label, serviceFee.label) &&
-                Objects.equals(value, serviceFee.value);
+                Objects.equals(amount, serviceFee.amount);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, locationId, name, created, taxRate, enabled, label, value);
+        return Objects.hash(id, name, amount);
     }
 
     /** The service fee's unique id. */
     @JsonInclude(Include.NON_NULL)
     public String id;
 
-    /** The locationId. */
-    @JsonInclude(Include.NON_NULL)
-    public String locationId;
-
     /** The service fee's name */
     @JsonInclude(Include.NON_NULL)
     public String name;
 
-    /** The service fee's creation timestamp. */
+    /** The service fee's amount value */
     @JsonInclude(Include.NON_NULL)
-    public Date created;
-
-    /** The service fee's tax rate. */
-    @JsonInclude(Include.NON_NULL)
-    public Double taxRate;
-
-    /** the service fee's enabled state **/
-    @JsonInclude(Include.NON_DEFAULT)
-    public Boolean enabled = Boolean.FALSE;
-
-    /** The service fee's label */
-    @JsonInclude(Include.NON_NULL)
-    public String label;
-
-    /** The service fee's value */
-    @JsonInclude(Include.NON_NULL)
-    public Value value;
+    public String amount;
 }
